dc335288ff1fe231d5d62f497112772532036bb8
kohlbrecher <kohlbrecher@sim.tu-darmstadt.de>
2015-06-15 19:27:59 +0200

    Clean up dependencies a little bit

--- hector_heat_detection/src/heat_detection.h
+++ hector_heat_detection/src/heat_detection.h
@@ -6,11 +6,9 @@
 #include <hector_worldmodel_msgs/ImagePercept.h>
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
-#include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/features2d/features2d.hpp>
-#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <sensor_msgs/image_encodings.h>
-#include <cv_bridge/cv_bridge.h>
 //#include <thermaleye_msgs/Mapping.h>
 
 #include <dynamic_reconfigure/server.h>

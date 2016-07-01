5864f57e776a2b501078530e1376158a602c78f0
Kevin Daun <kdaun@outlook.de>
2016-07-01 18:36:30 +0200

    fixed outputs that got introduced by revert

--- hector_detection_aggregator/src/detection_aggregator.cpp
+++ hector_detection_aggregator/src/detection_aggregator.cpp
@@ -39,7 +39,7 @@ void DetectionAggregator::updatePercepts()
     std::vector<std::string> keys;
     for(std::map<std::string, hector_perception_msgs::PerceptionDataArray>::iterator it = percept_storage_.begin(); it != percept_storage_.end(); ++it) {
         keys.push_back(it->first);
-        std::cout << it->first << "\n";
+        //std::cout << it->first << "\n";
     }
     for(std::string& key : keys)
     {
@@ -83,7 +83,7 @@ void DetectionAggregator::createImage()
                 const cv::Point *pts = (const cv::Point*) cv::Mat(polygon).data;
                 int npts = cv::Mat(polygon).rows;
 
-                ROS_INFO("Type: %s color %f %f %f ",percept_pair.first.c_str(),color_map_[percept_pair.first][0],color_map_[percept_pair.first][1],color_map_[percept_pair.first][2]);
+                //ROS_INFO("Type: %s color %f %f %f ",percept_pair.first.c_str(),color_map_[percept_pair.first][0],color_map_[percept_pair.first][1],color_map_[percept_pair.first][2]);
                 polylines(img_detected, &pts,&npts, 1,
                           true, 			// draw closed contour (i.e. joint end to start)
                           color_map_[percept_pair.first.c_str()],// colour RGB ordering (here = green)
@@ -109,7 +109,7 @@ void DetectionAggregator::createImage()
 void DetectionAggregator::imagePerceptCallback(const hector_perception_msgs::PerceptionDataArrayConstPtr& percept)
 {
     percept_storage_[percept->perceptionType]=(*percept);
-    ROS_INFO("Image Percept time %f",(float)(*percept).header.stamp.toSec());
+    //ROS_INFO("Image Percept time %f",(float)(*percept).header.stamp.toSec());
 }
 
 void DetectionAggregator::imageCallback(const sensor_msgs::ImageConstPtr& img) //, const sensor_msgs::CameraInfoConstPtr& info)

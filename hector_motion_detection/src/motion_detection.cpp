cf6a91e6b6c611937f0f5d6d40d41e0d38b61326
Martin Oehler <mail@martinoehler.de>
2017-07-25 06:09:02 +0900

    refactoring

--- hector_motion_detection/src/motion_detection.cpp
+++ hector_motion_detection/src/motion_detection.cpp
@@ -1,41 +1,38 @@
 //Author: Matej Zecevic
 #include "motion_detection.h"
 
-MotionDetection::MotionDetection()
-  : first_image_received_(false)
+MotionDetection::MotionDetection(ros::NodeHandle &nh)
+  : nh_(nh), first_image_received_(false)
 {
-    bg = cv::createBackgroundSubtractorMOG2();
+    bg_subtractor_ = cv::createBackgroundSubtractorMOG2();
 
-    ros::NodeHandle n;
-    ros::NodeHandle p_n("~"); //private nh
+    ros::NodeHandle pnh("~"); //private nh
 
-    p_n.param("enabled", enabled_, true);
+    pnh.param("enabled", enabled_, true);
 
-    image_transport::ImageTransport it(n);
+    image_transport::ImageTransport it(nh);
 
     image_sub_ = it.subscribe("image", 1 , &MotionDetection::imageCallback, this);
-    enabled_sub_ = p_n.subscribe("enabled", 10, &MotionDetection::enabledCallback, this);
+    enabled_sub_ = nh.subscribe("enabled", 10, &MotionDetection::enabledCallback, this);
+    enabled_pub_ = nh.advertise<std_msgs::Bool>("enabled_status", 10);
 
-    boost::bind(&MotionDetection::dynRecParamCallback, this, _1, _2);
     dyn_rec_type_ = boost::bind(&MotionDetection::dynRecParamCallback, this, _1, _2);
     dyn_rec_server_.setCallback(dyn_rec_type_);
 
-    image_percept_pub_ = n.advertise<hector_worldmodel_msgs::ImagePercept>("image_percept", 20);
+    image_percept_pub_ = nh.advertise<hector_worldmodel_msgs::ImagePercept>("image_percept", 20);
     image_motion_pub_ = it.advertiseCamera("image_motion", 10);
     image_detected_pub_ = it.advertiseCamera("image_detected", 10);
 
-    image_perception_pub = n.advertise<hector_perception_msgs::PerceptionDataArray>("detection/image_detection", 10);
+    image_perception_pub = nh.advertise<hector_perception_msgs::PerceptionDataArray>("detection/image_detection", 10);
     ROS_INFO("Starting with Motion Detection with MOG2");
-    ROS_INFO("debug_contours: %i", debug_contours);
-    ROS_INFO("shadows: %i", shadows);
-    ROS_INFO("max area: %d", max_area);
-    ROS_INFO("min area: %d", min_area);
-    ROS_INFO("detection limit: %d", detectionLimit);
-    image_transport::ImageTransport image_bg_it(p_n);
+    ROS_INFO("debug_contours: %i", debug_contours_);
+    ROS_INFO("shadows: %i", shadows_);
+    ROS_INFO("max area: %d", max_area_);
+    ROS_INFO("min area: %d", min_area_);
+    ROS_INFO("detection limit: %d", detectionLimit_);
+    image_transport::ImageTransport image_bg_it(pnh);
     image_background_subtracted_pub_ = image_bg_it.advertiseCamera("image_background_subtracted", 10);
 }
-
-MotionDetection::~MotionDetection() {}
 
 void MotionDetection::enabledCallback(const std_msgs::BoolConstPtr& enabled) {
   enabled_ = enabled->data;
@@ -48,42 +45,43 @@ void MotionDetection::imageCallback(const sensor_msgs::ImageConstPtr& img)
   }
   cv_bridge::CvImageConstPtr cv_ptr;
   cv_ptr = cv_bridge::toCvShare(img, sensor_msgs::image_encodings::BGR8);
-  cv::Mat img_filtered(cv_ptr->image);
+  cv::Mat frame(cv_ptr->image);
 
-  std::vector<std::vector<cv::Point> > contours;
-  cv::Mat frame, fgimg, fgimg_orig;
-
-  img_filtered.copyTo(frame);
-
+  cv::Mat fgimg;
   if (automatic_learning_rate_) {
-    bg->apply(img_filtered, fgimg);
+    bg_subtractor_->apply(frame, fgimg);
   } else {
-    bg->apply(img_filtered, fgimg, learning_rate_);
+    bg_subtractor_->apply(frame, fgimg, learning_rate_);
   }
+
+  cv::Mat fgimg_orig;
+  fgimg.copyTo(fgimg_orig);   //for debugging/tuning purposes
 
   cv::morphologyEx(fgimg, fgimg, cv::MORPH_CLOSE, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(3,3)));
 
-  fgimg.copyTo(fgimg_orig);   //for debugging/tuning purposes
-
   //controlable iterations for morphological operations
-  for(int i=0; i < erosion_iterations; i++) {
+  for(int i=0; i < erosion_iterations_; i++) {
     cv::erode(fgimg, fgimg, cv::Mat());
   }
-  for(int i=0; i < dilation_iterations; i++) {
+  for(int i=0; i < dilation_iterations_; i++) {
     cv::dilate(fgimg, fgimg, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(3,3)));
     //alternatively (tuning): other kernels: e.g. cv::getStructuringElement(cv::MORPH_RECT, cv::Size(10,10))
   }
 
+  // moving average of fg image
   if (!first_image_received_) {
     fgimg.copyTo(accumulated_image_);
     first_image_received_ = true;
   } else {
     cv::addWeighted(accumulated_image_, 0.9, fgimg, 0.1, 0.0, accumulated_image_);
   }
-  cv::Mat thres;
-  cv::threshold(accumulated_image_, thres, 100, 255, cv::THRESH_BINARY);
-  cv::findContours (thres, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
-  if (debug_contours) {
+  cv::Mat thresholded;
+  cv::threshold(accumulated_image_, thresholded, 100, 255, cv::THRESH_BINARY);
+
+  // Find contours
+  std::vector<std::vector<cv::Point> > contours;
+  cv::findContours (thresholded, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
+  if (debug_contours_) {
     cv::drawContours(frame, contours, -1, cv::Scalar (0, 0, 255), 2);
   }
 
@@ -93,15 +91,14 @@ void MotionDetection::imageCallback(const sensor_msgs::ImageConstPtr& img)
   cv::Rect bounding_rect;
   std::vector<double> areas( contours.size() );
 
-  for( int i = 0; i < contours.size(); i++ )
-  {
+  for( int i = 0; i < contours.size(); i++ ) {
     double area = cv::contourArea( contours[i] );  //  Find the area of contour
     areas[i] = area;
   }
 
   std::vector<hector_perception_msgs::PerceptionData> polygonGroup;
   if(contours.size() != 0) {
-    for(int k=0; k < detectionLimit; k++) {
+    for(int k=0; k < detectionLimit_; k++) {
       for(int j=0; j < areas.size(); j++) {
         if( areas[j] > largest_area ) {
           largest_area = areas[j];
@@ -109,11 +106,10 @@ void MotionDetection::imageCallback(const sensor_msgs::ImageConstPtr& img)
           bounding_rect = cv::boundingRect( contours[j] ); // Find the bounding rectangle for biggest contour
         }
       }
-      if(areas[largest_contour_index] >= min_area && areas[largest_contour_index] <= max_area) {
+      if(areas[largest_contour_index] >= min_area_ && areas[largest_contour_index] <= max_area_) {
         cv::rectangle( frame, bounding_rect.tl(), bounding_rect.br(), cv::Scalar( 0, 0, 255 ), 2, 8, 0 );
 
         //Create a Polygon consisting of Point32 points for publishing
-        //std::cout << "BR first: " << bounding_rect.tl() << "BR lasst: " << bounding_rect.br() << std::endl;
         geometry_msgs::Point32 p1, p2, p3, p4;
         p1.x = bounding_rect.tl().x;
         p1.y = bounding_rect.tl().y;
@@ -129,11 +125,9 @@ void MotionDetection::imageCallback(const sensor_msgs::ImageConstPtr& img)
         recPoints.push_back(p2);
         recPoints.push_back(p4);
         recPoints.push_back(p3);
-        //std::cout << "recPoints: " << recPoints[0] << recPoints[1] << recPoints[2] << recPoints[3] << std::endl;
 
         geometry_msgs::Polygon polygon;
         polygon.points = recPoints;
-        //std::cout << "Polygon: " << polygon.points[0] << polygon.points[1] << polygon.points[2] << polygon.points[3] << std::endl;
         hector_perception_msgs::PerceptionData perceptionData;
         perceptionData.percept_name = "motion" + boost::lexical_cast<std::string>(k);
         perceptionData.polygon = polygon;
@@ -157,7 +151,7 @@ void MotionDetection::imageCallback(const sensor_msgs::ImageConstPtr& img)
 
   if(image_background_subtracted_pub_.getNumSubscribers() > 0) {
     cv_bridge::CvImage cvImg;
-    fgimg_orig.copyTo(cvImg.image);
+    cvImg.image = fgimg_orig;
     cvImg.header = img->header;
     cvImg.encoding = sensor_msgs::image_encodings::MONO8;
     image_background_subtracted_pub_.publish(cvImg.toImageMsg(), info);
@@ -165,7 +159,7 @@ void MotionDetection::imageCallback(const sensor_msgs::ImageConstPtr& img)
 
   if(image_motion_pub_.getNumSubscribers() > 0) {
     cv_bridge::CvImage cvImg;
-    thres.copyTo(cvImg.image);
+    cvImg.image = thresholded;
     cvImg.header = img->header;
     cvImg.encoding = sensor_msgs::image_encodings::MONO8;
     image_motion_pub_.publish(cvImg.toImageMsg(), info);
@@ -173,24 +167,30 @@ void MotionDetection::imageCallback(const sensor_msgs::ImageConstPtr& img)
 
   if(image_detected_pub_.getNumSubscribers() > 0) {
     cv_bridge::CvImage cvImg;
-    frame.copyTo(cvImg.image);
+    cvImg.image = frame;
     cvImg.header = img->header;
     cvImg.encoding = sensor_msgs::image_encodings::BGR8;
     image_detected_pub_.publish(cvImg.toImageMsg(), info);
   }
 }
 
+void MotionDetection::publishEnableStatus() {
+  std_msgs::Bool bool_msg;
+  bool_msg.data = enabled_;
+  enabled_pub_.publish(bool_msg);
+}
+
 void MotionDetection::dynRecParamCallback(MotionDetectionConfig &config, uint32_t level)
 {
-  bg->setNMixtures(3);
-  shadows = config.motion_detect_shadows;
-  bg->setDetectShadows(shadows);
-  min_area = config.motion_detect_min_area;
-  max_area = config.motion_detect_max_area;
-  detectionLimit = config.motion_detect_detectionLimit;
-  debug_contours = config.motion_detect_debug_contours;
-  erosion_iterations = config.motion_detect_erosion;
-  dilation_iterations = config.motion_detect_dilation;
+  bg_subtractor_->setNMixtures(3);
+  shadows_ = config.motion_detect_shadows;
+  bg_subtractor_->setDetectShadows(shadows_);
+  min_area_ = config.motion_detect_min_area;
+  max_area_ = config.motion_detect_max_area;
+  detectionLimit_ = config.motion_detect_detectionLimit;
+  debug_contours_ = config.motion_detect_debug_contours;
+  erosion_iterations_ = config.motion_detect_erosion;
+  dilation_iterations_ = config.motion_detect_dilation;
   learning_rate_ = config.learning_rate;
   automatic_learning_rate_ = config.automatic_learning_rate;
 }
@@ -198,7 +198,18 @@ void MotionDetection::dynRecParamCallback(MotionDetectionConfig &config, uint32_
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "motion_detection");
-  MotionDetection md;
-  ros::spin();
-}
+  ros::NodeHandle nh;
+  ros::CallbackQueue fast_queue;
+  nh.setCallbackQueue(&fast_queue);
+  ros::AsyncSpinner async_spinner(1, &fast_queue);
+  async_spinner.start();
 
+  MotionDetection md(nh);
+  ros::spin();
+  ros::Rate rate(1.0);
+  while (ros::ok()) {
+    ros::spinOnce();
+    md.publishEnableStatus();
+    rate.sleep();
+  }
+}
--- hector_motion_detection/src/motion_detection.h
+++ hector_motion_detection/src/motion_detection.h
@@ -2,20 +2,20 @@
 #define _HECTOR_MOTION_DETECTION_H_
 
 #include <ros/ros.h>
+#include <ros/callback_queue.h>
+
+#include <image_transport/image_transport.h>
 #include <std_msgs/String.h>
-#include <opencv/cv.h>
-#include <cv_bridge/cv_bridge.h>
-#include <geometry_msgs/Polygon.h>
-#include <geometry_msgs/Point32.h>
+#include <std_msgs/Bool.h>
+#include <sensor_msgs/image_encodings.h>
+#include <hector_worldmodel_msgs/ImagePercept.h>
 #include <hector_perception_msgs/PerceptionDataArray.h>
 
-#include <hector_worldmodel_msgs/ImagePercept.h>
-#include <image_transport/image_transport.h>
+#include <opencv/cv.h>
 #include <cv_bridge/cv_bridge.h>
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui.hpp>
-#include <sensor_msgs/image_encodings.h>
-#include <std_msgs/Bool.h>
+
 
 #include <dynamic_reconfigure/server.h>
 #include <hector_motion_detection/MotionDetectionConfig.h>
@@ -24,12 +24,14 @@ using hector_motion_detection::MotionDetectionConfig;
 
 class MotionDetection{
 public:
-    MotionDetection();
-    ~MotionDetection();
+    MotionDetection(ros::NodeHandle& nh);
+    void publishEnableStatus();
 private:
     void imageCallback(const sensor_msgs::ImageConstPtr& img); //, const sensor_msgs::CameraInfoConstPtr& info);
     void enabledCallback(const std_msgs::BoolConstPtr& enabled);
     void dynRecParamCallback(MotionDetectionConfig &config, uint32_t level);
+
+    ros::NodeHandle nh_;
 
     bool enabled_;
 
@@ -37,6 +39,7 @@ private:
     ros::Publisher image_perception_pub;
     image_transport::Subscriber image_sub_;
     ros::Subscriber enabled_sub_;
+    ros::Publisher enabled_pub_;
 
     image_transport::CameraSubscriber camera_sub_;
     image_transport::CameraPublisher image_motion_pub_;
@@ -48,22 +51,20 @@ private:
     bool first_image_received_;
     cv::Mat accumulated_image_;
 
-    int motion_detect_threshold_;
-    double min_percept_size, max_percept_size;
-    double min_density;
-    std::string percept_class_id_;
-    double learning_rate_;
-    bool automatic_learning_rate_;
 
-    cv::Ptr<cv::BackgroundSubtractorMOG2> bg; //with regards to shadows
-    int detectionLimit; //the maximal number of detections to make/objects to track
-    int min_area; //to filter smaller areas
-    int max_area; //to filter bigger areas
+
+    cv::Ptr<cv::BackgroundSubtractorMOG2> bg_subtractor_; //with regards to shadows
+    int detectionLimit_; //the maximal number of detections to make/objects to track
+    int min_area_; //to filter smaller areas
+    int max_area_; //to filter bigger areas
+    double learning_rate_; // learning rate of bg subtractor
+    bool automatic_learning_rate_; // set learning rate automatically
+    int erosion_iterations_, dilation_iterations_; //for controlling the iterations of erosion/deliation
+    bool shadows_; //control if shadows should be tracked
+    bool debug_contours_;
 
     image_transport::CameraPublisher image_background_subtracted_pub_; //for publishing subtracted image
-    int erosion_iterations, dilation_iterations; //for controlling the iterations of erosion/deliation
-    bool shadows; //control if shadows should be tracked
-    bool debug_contours;
+
 
 };
 

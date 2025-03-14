e1f342a286bebb68eb1f7a1d094d4dec50caf111
Finn Winter <finn.winter@stud.tu-darmstadt.de>
2025-03-14 16:46:03 +0100

    Hector Detection Aggregator now functional

--- hector_detection_aggregator/include/hector_detection_aggregator/detection_aggregator.hpp
+++ hector_detection_aggregator/include/hector_detection_aggregator/detection_aggregator.hpp
@@ -10,6 +10,16 @@
 
 typedef vision_msgs::msg::Detection2DArray Detection2DArray;
 
+// TODO remove this if not needed
+template <>
+struct std::hash<std::pair<std::string, std::string>>
+{
+  size_t operator()(const std::pair<std::string, std::string>& p) const noexcept
+  {
+    const size_t hash = std::hash<std::string>()(p.first) ^ std::hash<std::string>()(p.second);
+    return hash;
+  }
+};
 namespace hector_detection_aggregator
 {
 class DetectionAggregator
@@ -23,7 +33,8 @@ public:
 
 private:
   void updateDetections();
-  void imageCallback(const sensor_msgs::msg::Image::ConstSharedPtr& img);
+  void imageCallback(const sensor_msgs::msg::Image::ConstSharedPtr& image,
+                     const sensor_msgs::msg::CameraInfo::ConstSharedPtr& camera_info);
   void imageDetectionCallback(const Detection2DArray::ConstSharedPtr& percept);
   // TODO Parameter update
   // void dynRecParamCallback(HectorDetectionAggregatorConfig &config, uint32_t level);
@@ -31,26 +42,31 @@ private:
   void connectCb();
   void startSubscribers();
   void stopSubscribers();
+  void publisherSubscriptionCallback();
 
   rclcpp::Node::SharedPtr node_;
   std::shared_ptr<rclcpp::ParameterEventHandler> param_subscriber_;
   std::shared_ptr<rclcpp::ParameterCallbackHandle> storage_duration_callback_handle_;
+  rclcpp::TimerBase::SharedPtr check_subscribers_timer_;
+
   std::shared_ptr<image_transport::ImageTransport> image_transport_;
 
   image_transport::CameraPublisher image_detected_pub_;
 
   rclcpp::Subscription<Detection2DArray>::SharedPtr image_percept_sub_;
-  image_transport::Subscriber image_sub_;
-  image_transport::CameraSubscriber camera_sub_;
+  image_transport::CameraSubscriber camera_subscriber_;
 
-  cv_bridge::CvImageConstPtr img_current_grey_ptr_;
-  cv_bridge::CvImageConstPtr img_current_col_ptr_;
+  cv_bridge::CvImageConstPtr current_color_image_;
+  // cv_bridge::CvImageConstPtr current_grey_image_;
+  sensor_msgs::msg::CameraInfo::ConstSharedPtr current_camera_info_;
 
   std::map<std::pair<std::string, std::string>, vision_msgs::msg::Detection2D> detection_map_;
   std::map<std::string, cv::Scalar> color_map_;
 
    //params
   rclcpp::Duration storage_duration_;
+
+  bool has_subscribers_;
 };
 }
 #endif
--- hector_detection_aggregator/src/detection_aggregator.cpp
+++ hector_detection_aggregator/src/detection_aggregator.cpp
@@ -8,12 +8,13 @@
 
 namespace hector_detection_aggregator
 {
-DetectionAggregator::DetectionAggregator(const rclcpp::Node::SharedPtr& node) : storage_duration_(0, 1e8)
+DetectionAggregator::DetectionAggregator(const rclcpp::Node::SharedPtr& node)
+  : storage_duration_(0, 1e8), has_subscribers_(false)
 {
   node_ = node;
   param_subscriber_ = std::make_shared<rclcpp::ParameterEventHandler>(node_);
 
-  node_->declare_parameter<double>("storage_duration", 0.1);
+  node_->declare_parameter<double>("storage_duration", 10.0);
   auto storage_duration_parameter_callback = [this](const  rclcpp::Parameter& p)
   {
     const double storage_duration_param = node_->get_parameter("storage_duration").as_double();
@@ -24,11 +25,12 @@ DetectionAggregator::DetectionAggregator(const rclcpp::Node::SharedPtr& node) : 
   storage_duration_callback_handle_ = param_subscriber_->add_parameter_callback("storage_duration", storage_duration_parameter_callback);
 
   // Color mappings A color for "unknown" is required to exist
-  color_map_["motion"] = cv::Scalar(0,0,255);     // Blue
-  color_map_["qr"] = cv::Scalar(255,0,0);         // Red
-  color_map_["heat"] = cv::Scalar(0,255,0);       // Green
-  color_map_["hazmat"] = cv::Scalar(255,255,0);   // Yellow
-  color_map_["unknown"] = cv::Scalar(50, 50, 50); // Grey
+  color_map_["motion"] = cv::Scalar(0,0,255);       // Red
+  color_map_["qr"] = cv::Scalar(255,0,0);           // Blue
+  color_map_["apriltag"] = cv::Scalar(216, 0, 134); // Purple
+  color_map_["heat"] = cv::Scalar(0,255,0);         // Green
+  color_map_["hazmat"] = cv::Scalar(255,255,0);     // Turquoise
+  color_map_["unknown"] = cv::Scalar(50, 50, 50);   // Grey
 
   image_transport_ = std::make_shared<image_transport::ImageTransport>(node_);
   image_transport::SubscriberStatusCallback connect_cb = std::bind(
@@ -37,14 +39,15 @@ DetectionAggregator::DetectionAggregator(const rclcpp::Node::SharedPtr& node) : 
   image_detected_pub_ = image_transport_->advertiseCamera(
     "/detection/aggregated_detections_image", 10);
 
-  img_current_col_ptr_.reset();
+  check_subscribers_timer_ = node_->create_wall_timer(std::chrono::seconds(1), std::bind(&DetectionAggregator::publisherSubscriptionCallback, this));
+
+  current_color_image_.reset();
   image_percept_sub_.reset();
 
   RCLCPP_INFO(node_->get_logger(), "Node started");
 }
 
 DetectionAggregator::~DetectionAggregator() = default;
-
 
 void DetectionAggregator::updateDetections()
 {
@@ -53,63 +56,67 @@ void DetectionAggregator::updateDetections()
     storage_threshold = node_->now() - storage_duration_;
   }
 
-  std::vector<std::string> keys;
-  for (auto it = detection_map_.begin(); it != detection_map_.end(); ++it)
+  for (auto it = detection_map_.begin(); it != detection_map_.end(); )
   {
-    if (it->second.header.stamp.sec < storage_threshold.seconds())
-      detection_map_.erase(it);
+    const auto current_it = it;
+    ++it;
+    if (current_it->second.header.stamp.sec < storage_threshold.seconds())
+    {
+      detection_map_.erase(current_it);
+    }
   }
 }
 
 void DetectionAggregator::createImage()
 {
-  if (image_detected_pub_.getNumSubscribers() == 0)
+  updateDetections();
+  RCLCPP_DEBUG(node_->get_logger(), "Creating detection Image for %lu detections", detection_map_.size());
+
+  if (!current_color_image_)
     return;
 
-  updateDetections();
+  cv::Mat img_detected;
+  current_color_image_->image.copyTo(img_detected);
 
-  if (img_current_col_ptr_ )
+  for (const auto& [id, detection] : detection_map_)
   {
-    cv::Mat img_detected;
-    img_current_col_ptr_->image.copyTo(img_detected);
+    const cv::Point detection_center_point(static_cast<int>(detection.bbox.center.position.x - detection.bbox.size_x / 2),
+                                           static_cast<int>(detection.bbox.center.position.y - detection.bbox.size_y / 2));
+    const cv::Size detection_size(static_cast<int>(detection.bbox.size_x),
+                                  static_cast<int>(detection.bbox.size_y));
 
-      for (const auto& [id, detection] : detection_map_)
-      {
-        const cv::Point detection_center_point(static_cast<int>(detection.bbox.center.position.x),
-                                               static_cast<int>(detection.bbox.center.position.y));
-        const cv::Size detection_size(static_cast<int>(detection.bbox.size_x),
-                                      static_cast<int>(detection.bbox.size_y));
+    const cv::Rect rect(detection_center_point, detection_size);
 
-        const cv::Rect rect(detection_center_point, detection_size);
+    // Detections of unknown type are colored grey
+    cv::Scalar detection_color;
+    const auto color_find = color_map_.find(id.first);
+    if (color_find == color_map_.end())
+      detection_color = color_map_["unknown"];
+    else
+      detection_color = color_find->second;
 
-        // Detections of unknown type are colored grey
-        cv::Scalar detection_color;
-        if (const auto color_find = color_map_.find(id.first); color_find == color_map_.end())
-          detection_color = color_map_["unknown"];
-        else
-          detection_color = color_find->second;
+    const std::string detection_text = id.first + ": " + id.second;
 
-        //ROS_INFO("Type: %s color %f %f %f ",percept_pair.first.c_str(),color_map_[percept_pair.first][0],color_map_[percept_pair.first][1],color_map_[percept_pair.first][2]);
-        cv::rectangle(img_detected, rect, detection_color, 2, cv::LINE_AA);
-        cv::putText(img_detected, id.first, detection_center_point,cv::FONT_HERSHEY_SIMPLEX, 0.75, detection_color, 2);
-      }
-
-    cv_bridge::CvImage cvImg;
-    img_detected.copyTo(cvImg.image);
-    //cvImg.header = img->header;
-    cvImg.encoding = sensor_msgs::image_encodings::BGR8;
-    const auto info = std::make_shared<sensor_msgs::msg::CameraInfo>();
-    // info->header = img->header; todo(kdaun) add info header
-    info->header.stamp = node_->now();
-
-    image_detected_pub_.publish(cvImg.toImageMsg(), info);
+    //ROS_INFO("Type: %s color %f %f %f ",percept_pair.first.c_str(),color_map_[percept_pair.first][0],color_map_[percept_pair.first][1],color_map_[percept_pair.first][2]);
+    cv::rectangle(img_detected, rect, detection_color, 2, cv::LINE_AA);
+    cv::putText(img_detected, detection_text, detection_center_point + cv::Point(0, -10), cv::FONT_HERSHEY_SIMPLEX, 0.75, detection_color, 2);
   }
+
+  cv_bridge::CvImage cvImg;
+  img_detected.copyTo(cvImg.image);
+  //cvImg.header = img->header;
+  cvImg.encoding = sensor_msgs::image_encodings::BGR8;
+  const auto info = std::make_shared<sensor_msgs::msg::CameraInfo>();
+  info->header = current_color_image_->header;
+
+  RCLCPP_DEBUG(node_->get_logger(), "Publishing detection image");
+
+  image_detected_pub_.publish(cvImg.toImageMsg(), current_camera_info_);
 }
 
 void DetectionAggregator::imageDetectionCallback(const Detection2DArray::ConstSharedPtr& percept)
 {
-  if(image_detected_pub_.getNumSubscribers() == 0)
-    return;
+  RCLCPP_DEBUG(node_->get_logger(), "Aggregating Perceptions");
 
   // Write all detections with identifications into the map
   for (const auto& detection : percept->detections)
@@ -132,13 +139,17 @@ void DetectionAggregator::imageDetectionCallback(const Detection2DArray::ConstSh
   }
 }
 
-void DetectionAggregator::imageCallback(const sensor_msgs::msg::Image::ConstSharedPtr& img) //, const sensor_msgs::CameraInfoConstPtr& info)
+void DetectionAggregator::imageCallback(const sensor_msgs::msg::Image::ConstSharedPtr& image,
+                                        const sensor_msgs::msg::CameraInfo::ConstSharedPtr& camera_info)
 {
   if(image_detected_pub_.getNumSubscribers() == 0)
     return;
 
-  //img_current_grey_ptr_ = cv_bridge::toCvShare(img, sensor_msgs::image_encodings::MONO8);
-  img_current_col_ptr_ = cv_bridge::toCvShare(img, sensor_msgs::image_encodings::BGR8);
+  // current_grey_image_ = cv_bridge::toCvShare(img, sensor_msgs::image_encodings::MONO8);
+  current_color_image_ = cv_bridge::toCvShare(image, sensor_msgs::image_encodings::BGR8);
+  current_camera_info_ = camera_info;
+
+  createImage();
 }
 
 void DetectionAggregator::connectCb()
@@ -154,7 +165,8 @@ void DetectionAggregator::connectCb()
 
 void DetectionAggregator::startSubscribers()
 {
-  image_sub_ = image_transport_->subscribe("/sensor_head_rgbd_cam/rgb/image_raw", 1 , &DetectionAggregator::imageCallback, this);
+  RCLCPP_INFO(node_->get_logger(), "Starting subscribers");
+  camera_subscriber_ = image_transport_->subscribeCamera("/image", 1, &DetectionAggregator::imageCallback, this);
   image_percept_sub_ = node_->create_subscription<Detection2DArray>(
     "/detection/visual_detection", 1,
     std::bind(&DetectionAggregator::imageDetectionCallback, this, std::placeholders::_1));
@@ -162,8 +174,29 @@ void DetectionAggregator::startSubscribers()
 
 void DetectionAggregator::stopSubscribers()
 {
-  image_sub_.shutdown();
+  RCLCPP_INFO(node_->get_logger(), "Stopping subscribers");
+  camera_subscriber_.shutdown();
   image_percept_sub_.reset();
+}
+
+void DetectionAggregator::publisherSubscriptionCallback()
+{
+  const size_t subscribers = image_detected_pub_.getNumSubscribers();
+
+  RCLCPP_DEBUG_STREAM(node_->get_logger(), "Subscribers: " << subscribers << " has_subscribers_: " << has_subscribers_);
+
+  // Changed to no subscribers
+  if (subscribers == 0 && has_subscribers_)
+  {
+    has_subscribers_ = false;
+    stopSubscribers();
+  }
+  // Changed from no subscribers
+  if (subscribers > 0 && !has_subscribers_)
+  {
+    has_subscribers_ = true;
+    startSubscribers();
+  }
 }
 }
 
@@ -173,7 +206,9 @@ int main( int argc, char **argv )
 
   const auto node = std::make_shared<rclcpp::Node>("detection_aggregator");
   auto detection_aggregator = hector_detection_aggregator::DetectionAggregator(node);
+
   rclcpp::spin( node );
+
   rclcpp::shutdown();
   return 0;
 }
